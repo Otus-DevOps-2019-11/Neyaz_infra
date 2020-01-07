07c8bb2a90fc8ce466473e8010b47f4763bf592a
Nikolay Malinin <ne9zzz@gmail.com>
2020-01-07 20:50:44 +0300

    Add packer template

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Import the public key used by the package management system.
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+
+# Create a list file for MongoDB.
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+# Install the MongoDB packages.
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+# Start MongoDB
+sudo systemctl start mongod
+sudo systemctl enable mongod
